095d93793e83d0ffbde09f2bab247d2cd03bdd87
刘骢 <2592509183@qq.com>
2021-01-20 21:00:44 +0800

    添加__packed&&修改id

--- protocol.h
+++ protocol.h
@@ -7,25 +7,25 @@
 #define AI_NOTICE_AUTOMATIC (1 << 2)
 #define AI_NOTICE_FIRE (1 << 3)
 
-#define AI_ID_MCU (0x01)
-#define AI_ID_REF (0x02)
+#define AI_ID_MCU (0xC4)
+#define AI_ID_REF (0xA8)
 
 #include <stdint.h>
 
 /* 电控 -> 视觉 MCU数据结构体*/
-typedef struct {
+typedef struct __packed {
   struct {
     float q0;
     float q1;
     float q2;
     float q3;
-  } quaternion; /* 四元数 */
+  } quat; /* 四元数 */
 
   uint8_t notice; /* 控制命令 */
 
   float ball_speed; /* 子弹初速度 */
 
-  struct {
+  struct __packed {
     float left;
     float right;
   } distance; /* 左右距离(哨兵) */
@@ -36,14 +36,14 @@ typedef struct {
 } Protocol_Data_MCU_t;
 
 /* 电控 -> 视觉 裁判系统数据结构体*/
-typedef struct {
+typedef struct __packed {
   uint16_t example;
 
   uint16_t crc16; /* crc校验 */
 } Protocol_Data_Referee_t;
 
 /* 视觉 -> 电控 数据结构体*/
-typedef struct {
+typedef struct __packed {
   struct {
     float yaw;    /* 偏航角（Yaw angle） */
     float pit;    /* 俯仰角（Pitch angle） */
@@ -57,13 +57,13 @@ typedef struct {
   uint16_t crc16; /* crc校验 */
 } Protocol_AI_t;
 
-typedef struct {
+typedef struct __packed {
   uint8_t id;
 
   Protocol_Data_MCU_t data;
 } Protocol_MCU_t;
 
-typedef struct {
+typedef struct __packed {
   uint8_t id;
 
   Protocol_Data_Referee_t data;
