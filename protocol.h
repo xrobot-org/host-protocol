02b7608f333cc64a03d04f602c308df53611cd76
Liu Zhongqi <3130676972@qq.com>
2022-02-12 20:20:17 +0800

    修改兵种信息位置

--- protocol.h
+++ protocol.h
@@ -10,14 +10,15 @@
 extern "C" {
 #endif
 
-#define AI_NOTICE_INFANTRY (1 << 0)
-#define AI_NOTICE_HERO (1 << 1)
-#define AI_NOTICE_ENGINEER (1 << 2)
-#define AI_NOTICE_DRONE (1 << 3)
-#define AI_NOTICE_SENTRY (1 << 4)
-#define AI_NOTICE_DART (1 << 5)
-#define AI_NOTICE_RADAR (1 << 6)
-#define AI_NOTICE_FIRE (1 << 7)
+#define AI_ARM_INFANTRY (1 << 0)
+#define AI_ARM_HERO (1 << 1)
+#define AI_ARM_ENGINEER (1 << 2)
+#define AI_ARM_DRONE (1 << 3)
+#define AI_ARM_SENTRY (1 << 4)
+#define AI_ARM_DART (1 << 5)
+#define AI_ARM_RADAR (1 << 6)
+
+#define AI_NOTICE_FIRE (1 << 1)
 
 #define AI_ID_MCU (0xC4)
 #define AI_ID_REF (0xA8)
@@ -60,22 +61,23 @@ typedef struct __attribute__((packed)) {
   uint8_t base_hp;       /* 基地血量 */
   uint8_t sentry_hp;     /* 哨兵血量 */
   uint8_t ballet_remain; /* 剩余弹量 */
+  uint8_t arm;           /* 兵种信息 */
 } Protocol_UpDataReferee_t;
 
 /* 视觉 -> 电控 数据结构体*/
 typedef struct __attribute__((packed)) {
   struct __attribute__((packed)) {
-    float yaw; /* 偏航角（Yaw angle） */
-    float pit; /* 俯仰角（Pitch angle） */
-    float rol; /* 翻滚角（Roll angle） */
+    float yaw; /* 偏航角(Yaw angle) */
+    float pit; /* 俯仰角(Pitch angle) */
+    float rol; /* 翻滚角(Roll angle) */
   } gimbal;    /* 欧拉角 */
 
   Protocol_ID_t notice; /* 控制命令 */
 
   struct __attribute__((packed)) {
     float vx;         /* x轴移动速度 */
-    float vy;         /* y轴移动速度、哨兵沿轨道方向（正面面向战场，右为正方向） */
-    float wz;         /* z轴转动速度、哨兵射界（弧度0～pi） */
+    float vy;         /* y轴移动速度、哨兵沿轨道方向(正面面向战场，右为正方向) */
+    float wz;         /* z轴转动速度、哨兵射界(弧度0～pi) */
   } chassis_move_vec; /* 底盘移动向量 */
 } Protocol_DownData_t;
 
